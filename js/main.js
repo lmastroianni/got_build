4bceba6c32c9567d56860147a86d9e29996b2998
Lucas Mastroianni <lcmhockey@hotmail.com>
2019-03-18 12:29:28 -0400

    more functionality

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 	const sigils   = document.querySelectorAll('.sigil-container');
 	      lightbox = document.querySelector('.lightbox');
 	      video    = document.querySelector('video');
+	      lbClose = document.querySelector(".lightbox-close");
 
 	function showLightbox() {
 		//debugger;
@@ -12,11 +13,10 @@
 		let targetHouse = this.className.split(" ")[1];
 		// this gives us back a lowercase house name
 		// flip this to uppercase
-		targetHouse = targetHouse.replace(targetHouse.charAt(0).
-			toUpperCase() + targetHouse.splice(1));
+		let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
 
 
-		let targetSource = `video/House-${targetHouse}.mp4`;
+		video.src = `video/House-${targetVid}.mp4`;
 		lightBox.classlist.add('show-lightbox');
 
 		video.load();
@@ -34,10 +34,9 @@
 
     }
 
-    shields.forEach(shield => shield.addEventListener('click, showLightbox'));
+	sigils.forEach(sigil => sigil.addEventListener('click', showLightbox));
 
-	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
-
-	video.addEventListener('ended', closeLightbox)
+	video.addEventListener('ended', hideLightbox);
+    lbClose.addEventListener('click', hideLightbox);
 
 })();

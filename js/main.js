63b7c617c495cd729a5bce6f45f8dc620b08fd4c
Lucas Mastroianni <lcmhockey@hotmail.com>
2019-03-25 12:18:58 -0400

    changing data one way

--- js/main.js
+++ js/main.js
@@ -2,11 +2,22 @@
 	// stub
 	console.log('fired');
 
-	const sigils     = document.querySelectorAll('.sigil-container');
-	      lightbox   = document.querySelector('.lightbox');
-	      video      = document.querySelector('video');
-	      lbClose    = document.querySelector(".lightbox-close");
-	      topBanners = document.querySelector('#houseImages');
+	const sigils     = document.querySelectorAll('.sigil-container'),
+	      lightbox   = document.querySelector('.lightbox'),
+	      video      = document.querySelector('video'),
+	      lbClose    = document.querySelector('.lightbox-close'),
+	      topBanners = document.querySelector('#houseImages'),
+	      tagline    = document.querySelector('.house-name'),
+	      houseInfo  = document.querySelector('.house-info');
+
+	      //this is a container for some dynamic data
+	const houseData = [ //STARK
+	//backtciks (js template string) let you keep punctuation
+	    `STARK
+House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.
+`
+
+	];
 
 	function showLightbox() {
 		//debugger;
@@ -15,6 +26,15 @@
 		// this gives us back a lowercase house name
 		// flip this to uppercase
 		let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		//populate the tagline
+		tagline.textContent = `House ${targetVid}`;
+
+		//populate houe content
+		houseInfo.textContent = houseData[0];
+
+
+
 
 
 		video.src = `video/House-${targetVid}.mp4`;
@@ -37,9 +57,9 @@
    function animateBanners() {
 	// move banners to left so current house banner is visible
 	const offSet = 600;
-	let currentOffset = this.dataset.offset * offset;
+	let currentOffset = this.dataset.offset * offSet;
 
-	topBanners.style.right = currentOffset + "px";
+	TweenMax.to(topBanners, 0.7, {right: currentOffset});
 
 
 }
